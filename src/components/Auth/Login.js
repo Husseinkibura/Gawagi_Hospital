b93df2084c3f87f906a085869eedb2ec31757927
Hussein Kibura <husseinkibura@gmail.com>
2025-03-17 04:00:24 +0300

    Updated the hospital management system files

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -148,7 +148,7 @@ const Login = () => {
     setLoading(true);
   
     try {
-      const response = await axios.post("http://localhost:5000/api/auth/login", {
+      const response = await axios.post("https://api.gawagidispensary.com/api/auth/login", {
         username,
         password,
       });
